55c0eb8b41126a50ca272ce54e00153fbbcd6ff6
Mohammad Azmi <mazmiagus2003@gmail.com>
2022-03-19 11:19:37 +0700

    feat: 3x3 can be rotated left

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -19,6 +19,16 @@ export class RotatingShape {
     return new RotatingShape(rotated);
   }
 
+  rotateLeft() {
+    const rotated = Array(this.dim).fill("");
+    for (let i = this.dim - 1; i >= 0; i--) {
+      for (let j = 0; j < this.dim; j++) {
+        rotated[this.dim - 1 - i] += this.shape[j][i];
+      }
+    }
+    return new RotatingShape(rotated);
+  }
+
   toString() {
     return this.shape.join("\n") + "\n";
   }
